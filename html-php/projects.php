8833c3d841092066b4089233159e6b11886d2a31
Mohammed Ali <alhiloempire@gmail.com>
2019-11-21 14:34:59 +0100

    .

--- html-php/projects.php
+++ html-php/projects.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <ul>
+                <li><a class="" href="employees.php">Employees</a></li>
+                <li><a class="" href="benefits.php">Benefits</a></li>
+                <li><a class="active" href="projects.php">Projects</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
