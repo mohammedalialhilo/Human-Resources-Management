5f44b2deb4077333831f00fdad49231cb3d1f501
Mohammed Ali <alhiloempire@gmail.com>
2019-11-22 10:16:38 +0100

    nav d

--- html-php/projects.php
+++ html-php/projects.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="../css/header.css">
-
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -16,6 +16,12 @@
                 <li><a class="" href="benefits.php">Benefits</a></li>
                 <li><a class="active" href="projects.php">Projects</a></li>
             </ul>
+            <div class="search-container">
+                <form action="">
+                    <input class="search" type="text" placeholder="Search.." name="search">
+                    <button class="searchBtn" type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
         </nav>
     </header>
 </body>
